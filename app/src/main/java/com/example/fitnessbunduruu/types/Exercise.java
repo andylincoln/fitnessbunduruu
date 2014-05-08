07f4835a731396cc0ee72539ff44674d28022ec9
tcohen1418 <tim.cohen1418@gmail.com>
2014-05-08 18:15:50 -0400

    Implement Parcelable for ExerciseGroup and Exercise

--- app/src/main/java/com/example/fitnessbunduruu/types/Exercise.java
+++ app/src/main/java/com/example/fitnessbunduruu/types/Exercise.java
@@ -1,14 +1,20 @@
+// Exercise.java
+// Data structure to hold characteristics of a particular exercise.
+
 package com.example.fitnessbunduruu.types;
 
-public class Exercise {
+import android.os.Parcel;
+import android.os.Parcelable;
 
-    public enum type { CARDIO , STRENGTH }
+public class Exercise implements Parcelable{
+    // Implement the Parcelable class so we can pass Exercise object instances between activities.
 
-    private type type;
+    public enum workoutType { CARDIO , STRENGTH }
+
+    private workoutType type;
     private String name;
 
     private int[] difficulty_settings;
-
 
     /**
      *
@@ -18,21 +24,49 @@ public class Exercise {
      * @param medium
      * @param hard
      */
-    public Exercise(type type, String name, int easy, int medium, int hard) {
+    public Exercise(workoutType type, String name, int easy, int medium, int hard) {
 
         this.type = type;
         this.name = name;
 
         difficulty_settings = new int[] { easy, medium, hard };
+    }
+
+    // BEGIN FUNCTIONS REQUIRED TO IMPLEMENT PARCELABLE.
+    private Exercise(Parcel in) {
+        // This constructor is called in the activity receiving the Exercise object.
+        // Unpackage the data.
+        this.name = in.readString();
 
     }
+
+    public int describeContents() {
+        return 0;
+    }
+
+    public void writeToParcel(Parcel out, int flags) {
+        // This overloaded function is called in the activity sending the Exercise object.
+        // Package the data for transport.
+        out.writeString(this.name);
+    }
+
+
+    public static final Parcelable.Creator<Exercise> CREATOR
+            = new Parcelable.Creator<Exercise>() {
+        public Exercise createFromParcel(Parcel in) {
+            return new Exercise(in);
+        }
+
+        public Exercise[] newArray(int size) {
+            return new Exercise[size];
+        }
+    }; // END FUNCTIONS REQUIRED TO IMPLEMENT PARCELABLE.
 
     public String getName() {
         return this.name;
     }
 
-
-    public type getType() {
+    public workoutType getType() {
         return this.type;
     }
 
@@ -48,7 +82,6 @@ public class Exercise {
         return this.difficulty_settings[2];
     }
 
-
     public void setEasyLength(int newLength) {
         this.difficulty_settings[0] = newLength;
     }
@@ -60,6 +93,4 @@ public class Exercise {
     public void setHardLength(int newLength) {
         this.difficulty_settings[2] = newLength;
     }
-
-
 }
--- app/src/main/java/com/example/fitnessbunduruu/types/ExerciseGroup.java
+++ app/src/main/java/com/example/fitnessbunduruu/types/ExerciseGroup.java
@@ -1,13 +1,19 @@
+// ExerciseGroup.java
+// Data structure to manage a series of different exercises.
+
 package com.example.fitnessbunduruu.types;
+
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import java.util.ArrayList;
 
-public class ExerciseGroup {
+public class ExerciseGroup implements Parcelable {
+    // Implement the Parcelable class so that we can pass ExerciseGroup object instances between activities.
 
     private String name;
 
-
-    private final ArrayList<Exercise> list;
+    private ArrayList<Exercise> list;
 
     public ExerciseGroup(String name) {
 
@@ -15,6 +21,39 @@ public class ExerciseGroup {
         this.list = new ArrayList<Exercise>();
 
     }
+
+    // BEGIN FUNCTIONS REQUIRED TO IMPLEMENT PARCELABLE.
+    private ExerciseGroup(Parcel in) {
+        // This constructor is called in the activity receiving the ExerciseGroup object.
+        // Unpackage the data.
+
+        name = in.readString();
+        this.list = in.readArrayList(Exercise.class.getClassLoader());
+
+    }
+
+    public int describeContents() {
+        return 0;
+    }
+
+    public void writeToParcel(Parcel out, int flags) {
+        // This overloaded function is called in the activity sending the Exercise object.
+        // Package the data for transport.
+
+        out.writeString(this.name);
+        out.writeList(this.list);
+    }
+
+    public static final Parcelable.Creator<ExerciseGroup> CREATOR
+            = new Parcelable.Creator<ExerciseGroup>() {
+        public ExerciseGroup createFromParcel(Parcel in) {
+            return new ExerciseGroup(in);
+        }
+
+        public ExerciseGroup[] newArray(int size) {
+            return new ExerciseGroup[size];
+        }
+    }; // END FUNCTIONS REQUIRED TO IMPLEMENT PARCELABLE.
 
     public void add(Exercise exercise) {
         this.list.add(exercise);
@@ -39,6 +78,4 @@ public class ExerciseGroup {
     public int getSize() {
         return this.list.size();
     }
-
-
 }
